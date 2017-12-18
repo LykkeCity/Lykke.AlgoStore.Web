e8d25389c787320cb66783c9d26d49afd7aae426
dejan.stevanovic <dejan.stevanovic@primeholding.com>
2017-12-18 10:23:48 +0100

    Fixing refreshing issue attempt VIII

--- AngularApplication/src/app/app.module.ts
+++ AngularApplication/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 // Components
 import { AppComponent } from './app.component';
@@ -93,7 +94,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
-    }
+    },
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
