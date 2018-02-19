e75f64e1592cb25bd6b30ef7eb7d09d41acb25aa
Todor Ivanov <todor.ivanov@primeholding.com>
2018-02-19 15:26:35 +0200

    Add balances for wallets

--- AngularApplication/src/app/models/wallet.model.ts
+++ AngularApplication/src/app/models/wallet.model.ts
@@ -1,7 +1,10 @@
+import { Balance } from './balance.model';
+
 export class Wallet {
   Id?: string;
   Name?: string;
   Type?: string;
   Description?: string;
   ApiKey?: string;
+  Balances?: Balance[];
 }
