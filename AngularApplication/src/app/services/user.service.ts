930d95431db3d4cb33d38e475aee47f6668937e0
Todor Ivanov <todor.ivanov@primeholding.com>
2018-02-22 14:57:52 +0200

    Remove unused subject

--- AngularApplication/src/app/services/user.service.ts
+++ AngularApplication/src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { UserData } from '../models/userdata.interface';
 import { AuthRequestService } from './auth-request.service';
@@ -8,16 +7,12 @@ import { AuthRequestService } from './auth-request.service';
 @Injectable()
 export class UserService {
 
-  userInfo = new BehaviorSubject<UserData | null>(null);
-
   constructor(private authRequestService: AuthRequestService) {  }
 
   getUserInfo(): Observable<UserData> {
     return this.authRequestService.get('devapi/PersonalData').pipe(
       map(res => res['Result'])
-    ).pipe(tap(res => {
-      this.userInfo.next(res);
-    }));
+    );
   }
 
 }
