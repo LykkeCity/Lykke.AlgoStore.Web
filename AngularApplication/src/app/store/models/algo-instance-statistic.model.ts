8fed04ab8327a3513767fbf2c5c166e1dd66585e
Todor Ivanov <todor.ivanov@primeholding.com>
2018-03-01 12:06:48 +0200

    Add dummy data for statistics, trades and logs

---
+++ AngularApplication/src/app/store/models/algo-instance-statistic.model.ts
@@ -0,0 +1,95 @@
+export interface InstanceStatistic {
+  Id?: string;
+  Name?: string;
+  Value?: string;
+}
+
+export function getStats() {
+  return  [
+    {
+      Id: "ID1",
+      Name: "Fiat",
+      Value: "20"
+    },
+    {
+      Id: "ID2",
+      Name: "Crypto",
+      Value: "20"
+    },
+    {
+      Id: "ID3",
+      Name: "Start Balance",
+      Value: "2000"
+    },
+    {
+      Id: "ID4",
+      Name: "Net Profit",
+      Value: "20"
+    },
+    {
+      Id: "ID5",
+      Name: "Total number of trades",
+      Value: "20"
+    },
+    {
+      Id: "ID3",
+      Name: "Start Balance",
+      Value: "2000"
+    },
+    {
+      Id: "ID4",
+      Name: "Net Profit",
+      Value: "20"
+    },
+    {
+      Id: "ID5",
+      Name: "Total number of trades",
+      Value: "20"
+    },
+    {
+      Id: "ID3",
+      Name: "Start Balance",
+      Value: "2000"
+    },
+    {
+      Id: "ID4",
+      Name: "Net Profit",
+      Value: "20"
+    },
+    {
+      Id: "ID5",
+      Name: "Total number of trades",
+      Value: "20"
+    },
+    {
+      Id: "ID3",
+      Name: "Start Balance",
+      Value: "2000"
+    },
+    {
+      Id: "ID4",
+      Name: "Net Profit",
+      Value: "20"
+    },
+    {
+      Id: "ID5",
+      Name: "Total number of trades",
+      Value: "20"
+    },
+    {
+      Id: "ID3",
+      Name: "Start Balance",
+      Value: "2000"
+    },
+    {
+      Id: "ID4",
+      Name: "Net Profit",
+      Value: "20"
+    },
+    {
+      Id: "ID5",
+      Name: "Total number of trades",
+      Value: "20"
+    },
+  ];
+}
