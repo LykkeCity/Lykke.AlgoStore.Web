b9859abec4cbe3aca339662088c808cd115a0a2b
Todor Ivanov <todor.ivanov@primeholding.com>
2018-03-20 15:41:58 +0200

    Update snippets

--- AngularApplication/src/assets/editor-snippets/csharp.js
+++ AngularApplication/src/assets/editor-snippets/csharp.js
@@ -1,145 +1,321 @@
 define("ace/snippets/csharp",["require","exports","module"], function(require, exports, module) {
 "use strict";
 
-exports.snippetText = '## Access Modifiers\n\
-snippet po\n\
-	protected\n\
-snippet pu\n\
-	public\n\
-snippet pr\n\
-	private\n\
-##\n\
-## Class\n\
-snippet cl\n\
-	class ${1:Name} {\n\
-	}\n\
-snippet in\n\
-	interface ${1} :${2:Parent} {\n\
-	}\n\
-snippet tc\n\
-	public class ${1} : ${2:TestCase}\n\
-##\n\
-## Class Enhancements\n\
-snippet ext\n\
-	: \n\
-snippet imp\n\
-	:\n\
-##\n\
-## Comments\n\
-snippet /*\n\
-	/*\n\
-	 * ${1}\n\
-	 */\n\
-##\n\
-## Constants\n\
-snippet co\n\
-	static public const ${1:String} ${2:var} = ${3};${4}\n\
-snippet cos\n\
-	static public const String ${1:var} = "${2}";${3}\n\
-##\n\
-## Control Statements\n\
-snippet case\n\
-	case ${1}:\n\
-		${2}\n\
-snippet def\n\
-	default:\n\
-		${2}\n\
-snippet el\n\
-	else\n\
-snippet elif\n\
-	else if (${1}) ${2}\n\
-snippet if\n\
-	if (${1}) {\n\
-	}\n\
-snippet sw\n\
-	switch (${1}) {\n\
-		${2}\n\
-	}\n\
-##\n\
-## Create a Method\n\
-snippet m\n\
-	${1:void} ${2:method}(${3})${4}\n\
-##\n\
-## Create a Variable\n\
-snippet v\n\
-	${1:string} ${2:var}${3: = null}${4};${5}\n\
-##\n\
-## Enhancements to Methods, variables, classes, etc.\n\
-snippet ab\n\
-	abstract\n\
-snippet co\n\
-	const\n\
-snippet st\n\
-	static\n\
-snippet as\n\
-	async\n\
-##\n\
-## Exception Handling\n\
-snippet ca\n\
-	catch(${1:Exception} ${2:e}) ${3}\n\
-snippet thr\n\
-	throw\n\
-snippet ths\n\
-	throws\n\
-snippet try\n\
-	try {\n\
-		${3}\n\
-	} catch(${1:Exception} ${2:e}) {\n\
-	}\n\
-snippet tryf\n\
-	try {\n\
-		${3}\n\
-	} catch(${1:Exception} ${2:e}) {\n\
-	} finally {\n\
-	}\n\
-##\n\
-## Loops\n\
-snippet enfor\n\
-	for (${1} : ${2}) ${3}\n\
-snippet for\n\
-	for (int ${1}=0; ${1} < ${2}.Length; ${1}++;) {\n\
-	}\n\
-snippet wh\n\
-	while (${1}) ${2}\n\
-##\n\
-## Main method\n\
-snippet main\n\
-	public static void Main (string[] args) {\n\
-		${1:/* code */}\n\
-	}\n\
-##\n\
-## Terminate Methods or Loops\n\
-snippet re\n\
-	return\n\
-snippet br\n\
-	break;\n\
-##\n\
-## Test Methods\n\
-snippet t\n\
-	public void test${1:Name}() throws Exception {\n\
-		${2}\n\
-	}\n\
-snippet test\n\
-	@Test\n\
-	public void test${1:Name}() throws Exception {\n\
-		${2}\n\
-	}\n\
-##\n\
-## Methods\n\
-snippet meth\n\
-  public void ${1}()  {\n\
-    throw new NotImplementedException();\n\
-  }\n\
-snippet vmeth\n\
-  public virtual void ${1}()  {\n\
-    throw new NotImplementedException();\n\
-  }\n\
-snippet smeth\n\
-  public static void ${1}()  {\n\
-    throw new NotImplementedException();\n\
-  }\n\
-##\n\
-';
+exports.snippetText = 'snippet sim\n' +
+  '\tpublic static int Main(string[] args) {\n' +
+  '\t\t${1}\n' +
+  '\t\treturn 0;\n' +
+  '\t} \n' +
+  'snippet simc\n' +
+  '\tpublic class Application {\n' +
+  '\t\tpublic static int Main(string[] args) {\n' +
+  '\t\t\t${1}\n' +
+  '\t\t\treturn 0;\n' +
+  '\t\t}\n' +
+  '\t}\n' +
+  '# if condition\n' +
+  'snippet if\n' +
+  '\tif (${1}) {\n' +
+  '\t\t${2}\n' +
+  '\t} \n' +
+  'snippet el\n' +
+  '\telse {\n' +
+  '\t\t${1}\n' +
+  '\t} \n' +
+  'snippet ifs\n' +
+  '\tif (${1}) \n' +
+  '\t\t${2}\n' +
+  '# ternary conditional\n' +
+  'snippet t\n' +
+  '\t${1} ? ${2} : ${3}\n' +
+  'snippet ?\n' +
+  '\t${1} ? ${2} : ${3}\n' +
+  '# do while loop\n' +
+  'snippet do\n' +
+  '\tdo {\n' +
+  '\t\t${2}\n' +
+  '\t} while (${1});\n' +
+  '# while loop\n' +
+  'snippet wh\n' +
+  '\twhile (${1}) {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# for loop\n' +
+  'snippet for\n' +
+  '\tfor (int ${1:i} = 0; $1 < ${2:count}; $1${3:++}) {\n' +
+  '\t\t${4}\n' +
+  '\t}\n' +
+  '# foreach\n' +
+  'snippet fore\n' +
+  '\tforeach (var ${1:entry} in ${2}) {\n' +
+  '\t\t${3}\n' +
+  '\t}\n' +
+  'snippet foreach\n' +
+  '\tforeach (var ${1:entry} in ${2}) {\n' +
+  '\t\t${3}\n' +
+  '\t}\n' +
+  'snippet each\n' +
+  '\tforeach (var ${1:entry} in ${2}) {\n' +
+  '\t\t${3}\n' +
+  '\t}\n' +
+  '# interfaces\n' +
+  'snippet interface\n' +
+  '\tpublic interface ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet if+\n' +
+  '\tpublic interface ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# class bodies\n' +
+  'snippet class\n' +
+  '\tpublic class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls\n' +
+  '\t${2:public} class ${1:`Filename()`} {\n' +
+  '\t\t${3}\n' +
+  '\t}\n' +
+  'snippet cls+\n' +
+  '\tpublic class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls+^\n' +
+  '\tpublic static class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls&\n' +
+  '\tinternal class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls&^\n' +
+  '\tinternal static class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls|\n' +
+  '\tprotected class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  'snippet cls|%\n' +
+  '\tprotected abstract class ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# constructor\n' +
+  'snippet ctor\n' +
+  '\tpublic ${1:`Filename()`}() {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# properties - auto properties by default.\n' +
+  '# default type is int with layout get / set.\n' +
+  'snippet prop\n' +
+  '\t${1:public} ${2:int} ${3:} { get; set; }${4}\n' +
+  'snippet p\n' +
+  '\t${1:public} ${2:int} ${3:} { get; set; }${4}\n' +
+  'snippet p+\n' +
+  '\tpublic ${1:int} ${2:} { get; set; }${3}\n' +
+  'snippet p+&\n' +
+  '\tpublic ${1:int} ${2:} { get; internal set; }${3}\n' +
+  'snippet p+|\n' +
+  '\tpublic ${1:int} ${2:} { get; protected set; }${3}\n' +
+  'snippet p+-\n' +
+  '\tpublic ${1:int} ${2:} { get; private set; }${3}\n' +
+  'snippet p&\n' +
+  '\tinternal ${1:int} ${2:} { get; set; }${3}\n' +
+  'snippet p&|\n' +
+  '\tinternal ${1:int} ${2:} { get; protected set; }${3}\n' +
+  'snippet p&-\n' +
+  '\tinternal ${1:int} ${2:} { get; private set; }${3}\n' +
+  'snippet p|\n' +
+  '\tprotected ${1:int} ${2:} { get; set; }${3}\n' +
+  'snippet p|-\n' +
+  '\tprotected ${1:int} ${2:} { get; private set; }${3}\n' +
+  'snippet p-\n' +
+  '\tprivate ${1:int} ${2:} { get; set; }${3}\n' +
+  '# property - bool\n' +
+  'snippet pi\n' +
+  '\t${1:public} int ${2:} { get; set; }${3}\n' +
+  'snippet pi+\n' +
+  '\tpublic int ${1} { get; set; }${2}\n' +
+  'snippet pi+&\n' +
+  '\tpublic int ${1} { get; internal set; }${2}\n' +
+  'snippet pi+|\n' +
+  '\tpublic int ${1} { get; protected set; }${2}\n' +
+  'snippet pi+-\n' +
+  '\tpublic int ${1} { get; private set; }${2}\n' +
+  'snippet pi&\n' +
+  '\tinternal int ${1} { get; set; }${2}\n' +
+  'snippet pi&|\n' +
+  '\tinternal int ${1} { get; protected set; }${2}\n' +
+  'snippet pi&-\n' +
+  '\tinternal int ${1} { get; private set; }${2}\n' +
+  'snippet pi|\n' +
+  '\tprotected int ${1} { get; set; }${2}\n' +
+  'snippet pi|-\n' +
+  '\tprotected int ${1} { get; private set; }${2}\n' +
+  'snippet pi-\n' +
+  '\tprivate int ${1} { get; set; }${2}\n' +
+  '# property - bool\n' +
+  'snippet pb\n' +
+  '\t${1:public} bool ${2:} { get; set; }${3}\n' +
+  'snippet pb+\n' +
+  '\tpublic bool ${1} { get; set; }${2}\n' +
+  'snippet pb+&\n' +
+  '\tpublic bool ${1} { get; internal set; }${2}\n' +
+  'snippet pb+|\n' +
+  '\tpublic bool ${1} { get; protected set; }${2}\n' +
+  'snippet pb+-\n' +
+  '\tpublic bool ${1} { get; private set; }${2}\n' +
+  'snippet pb&\n' +
+  '\tinternal bool ${1} { get; set; }${2}\n' +
+  'snippet pb&|\n' +
+  '\tinternal bool ${1} { get; protected set; }${2}\n' +
+  'snippet pb&-\n' +
+  '\tinternal bool ${1} { get; private set; }${2}\n' +
+  'snippet pb|\n' +
+  '\tprotected bool ${1} { get; set; }${2}\n' +
+  'snippet pb|-\n' +
+  '\tprotected bool ${1} { get; private set; }${2}\n' +
+  'snippet pb-\n' +
+  '\tprivate bool ${1} { get; set; }${2}\n' +
+  '# property - string\n' +
+  'snippet ps\n' +
+  '\t${1:public} string ${2:} { get; set; }${3}\n' +
+  'snippet ps+\n' +
+  '\tpublic string ${1} { get; set; }${2}\n' +
+  'snippet ps+&\n' +
+  '\tpublic string ${1} { get; internal set; }${2}\n' +
+  'snippet ps+|\n' +
+  '\tpublic string ${1} { get; protected set; }${2}\n' +
+  'snippet ps+-\n' +
+  '\tpublic string ${1} { get; private set; }${2}\n' +
+  'snippet ps&\n' +
+  '\tinternal string ${1} { get; set; }${2}\n' +
+  'snippet ps&|\n' +
+  '\tinternal string ${1} { get; protected set; }${2}\n' +
+  'snippet ps&-\n' +
+  '\tinternal string ${1} { get; private set; }${2}\n' +
+  'snippet ps|\n' +
+  '\tprotected string ${1} { get; set; }${2}\n' +
+  'snippet ps|-\n' +
+  '\tprotected string ${1} { get; private set; }${2}\n' +
+  'snippet ps-\n' +
+  '\tprivate string ${1} { get; set; }${2}\n' +
+  '# members - void\n' +
+  'snippet m\n' +
+  '\t${1:public} ${2:void} ${3:}(${4:}) {\n' +
+  '\t\t${5:}\n' +
+  '\t}\n' +
+  'snippet m+\n' +
+  '\tpublic ${1:void} ${2:}(${3:}) {\n' +
+  '\t\t${4:}\n' +
+  '\t}\n' +
+  'snippet m&\n' +
+  '\tinternal ${1:void} ${2:}(${3:}) {\n' +
+  '\t\t${4:}\n' +
+  '\t}\n' +
+  'snippet m|\n' +
+  '\tprotected ${1:void} ${2:}(${3:}) {\n' +
+  '\t\t${4:}\n' +
+  '\t}\n' +
+  'snippet m-\n' +
+  '\tprivate ${1:void} ${2:}(${3:}) {\n' +
+  '\t\t${4:}\n' +
+  '\t}\n' +
+  '# members - int\n' +
+  'snippet mi\n' +
+  '\t${1:public} int ${2:}(${3:}) {\n' +
+  '\t\t${4:return 0;}\n' +
+  '\t}\n' +
+  'snippet mi+\n' +
+  '\tpublic int ${1:}(${2:}) {\n' +
+  '\t\t${3:return 0;}\n' +
+  '\t}\n' +
+  'snippet mi&\n' +
+  '\tinternal int ${1:}(${2:}) {\n' +
+  '\t\t${3:return 0;}\n' +
+  '\t}\n' +
+  'snippet mi|\n' +
+  '\tprotected int ${1:}(${2:}) {\n' +
+  '\t\t${3:return 0;}\n' +
+  '\t}\n' +
+  'snippet mi-\n' +
+  '\tprivate int ${1:}(${2:}) {\n' +
+  '\t\t${3:return 0;}\n' +
+  '\t}\n' +
+  '# members - bool\n' +
+  'snippet mb\n' +
+  '\t${1:public} bool ${2:}(${3:}) {\n' +
+  '\t\t${4:return false;}\n' +
+  '\t}\n' +
+  'snippet mb+\n' +
+  '\tpublic bool ${1:}(${2:}) {\n' +
+  '\t\t${3:return false;}\n' +
+  '\t}\n' +
+  'snippet mb&\n' +
+  '\tinternal bool ${1:}(${2:}) {\n' +
+  '\t\t${3:return false;}\n' +
+  '\t}\n' +
+  'snippet mb|\n' +
+  '\tprotected bool ${1:}(${2:}) {\n' +
+  '\t\t${3:return false;}\n' +
+  '\t}\n' +
+  'snippet mb-\n' +
+  '\tprivate bool ${1:}(${2:}) {\n' +
+  '\t\t${3:return false;}\n' +
+  '\t}\n' +
+  '# members - string\n' +
+  'snippet ms\n' +
+  '\t${1:public} string ${2:}(${3:}) {\n' +
+  '\t\t${4:return "";}\n' +
+  '\t}\n' +
+  'snippet ms+\n' +
+  '\tpublic string ${1:}(${2:}) {\n' +
+  '\t\t${3:return "";}\n' +
+  '\t}\n' +
+  'snippet ms&\n' +
+  '\tinternal string ${1:}(${2:}) {\n' +
+  '\t\t${3:return "";}\n' +
+  '\t}\n' +
+  'snippet ms|\n' +
+  '\tprotected string ${1:}(${2:}) {\n' +
+  '\t\t${3:return "";}\n' +
+  '\t}\n' +
+  'snippet ms-\n' +
+  '\tprivate string ${1:}(${2:}) {\n' +
+  '\t\t${3:return "";}\n' +
+  '\t}\n' +
+  '# structure\n' +
+  'snippet struct\n' +
+  '\tpublic struct ${1:`Filename()`} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# enumeration\n' +
+  'snippet enum\n' +
+  '\tpublic enum ${1} {\n' +
+  '\t\t${2}\n' +
+  '\t}\n' +
+  '# preprocessor directives\n' +
+  'snippet #if\n' +
+  '\t#if\n' +
+  '\t\t${1}\n' +
+  '\t#endif\n' +
+  '# inline xml documentation\n' +
+  'snippet ///\n' +
+  '\t/// <summary>\n' +
+  '\t/// ${1}\n' +
+  '\t/// </summary>\n' +
+  'snippet <p\n' +
+  '\t<param name="${1}">${2:$1}</param>${3}\n' +
+  'snippet <ex\n' +
+  '\t<exception cref="${1:System.Exception}">${2}</exception>${3}\n' +
+  'snippet <r\n' +
+  '\t<returns>${1}</returns>{${2}\n' +
+  'snippet <s\n' +
+  '\t<see cref="${1}"/>${2}\n' +
+  'snippet <rem\n' +
+  '\t<remarks>${1}</remarks>${2}\n' +
+  'snippet <c\n' +
+  '\t<code>${1}</code>${2}';
 exports.scope = "csharp";
 
 });
