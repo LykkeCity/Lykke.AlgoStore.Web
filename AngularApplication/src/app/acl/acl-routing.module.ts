78dc3a1f75bc9e945967bf6c27d4c9deb754d1c8
Todor Ivanov <todor.ivanov@primeholding.com>
2018-04-27 14:24:32 +0300

    Update routings with constant permissions

--- AngularApplication/src/app/acl/acl-routing.module.ts
+++ AngularApplication/src/app/acl/acl-routing.module.ts
@@ -1,5 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../services/auth-guard';
 import { AclComponent } from './acl.component';
 import { UsersListComponent } from './users-list/users-list.component';
@@ -7,16 +7,36 @@ import { UserRolesComponent } from './user-roles/user-roles.component';
 import { RolesListComponent } from './roles-list/roles-list.component';
 import { EditRoleComponent } from './edit-role/edit-role.component';
 import { ACLGuard } from '../services/acl.guard';
+import Permissions from '../store/models/permissions';
 
 
 const routes: Routes = [
-  { path: '', component: AclComponent, canActivate: [AuthGuard],
+  {
+    path: '', component: AclComponent, canActivate: [AuthGuard],
     children: [
-      { path: 'users', component: UsersListComponent, canActivate: [AuthGuard, ACLGuard], data: {acl: ['GetAllUsersWithRoles']} },
-      { path: 'users/:id', component: UserRolesComponent, canActivate: [AuthGuard, ACLGuard], data: {acl: ['GetUserByIdWithRoles']} },
-      { path: 'roles', component: RolesListComponent, canActivate: [AuthGuard, ACLGuard], data: {acl: ['GetAllUserRoles'] }},
-      { path: 'roles-edit', component: EditRoleComponent, canActivate: [AuthGuard, ACLGuard], data: {acl: ['GetAllPermissions', 'GetRoleById', 'GetPermissionsByRoleId']} },
-      { path: 'roles-edit/:id', component: EditRoleComponent, canActivate: [AuthGuard, ACLGuard], data: {acl: ['GetAllPermissions', 'GetRoleById']} }
+      {
+        path: 'users',
+        component: UsersListComponent,
+        canActivate: [AuthGuard, ACLGuard],
+        data: { acl: [Permissions.GET_ALL_USERS_WITH_ROLES] }
+      },
+      {
+        path: 'users/:id',
+        component: UserRolesComponent,
+        canActivate: [AuthGuard, ACLGuard],
+        data: { acl: [Permissions.GET_USER_BY_ID_WITH_ROLES] }
+      },
+      { path: 'roles', component: RolesListComponent, canActivate: [AuthGuard, ACLGuard], data: { acl: [Permissions.GET_ALL_USER_ROLES] } },
+      {
+        path: 'roles-edit', component: EditRoleComponent, canActivate: [AuthGuard, ACLGuard],
+        data: { acl: [Permissions.GET_ALL_PERMISSIONS, Permissions.GET_ROLE_BY_ID, Permissions.GET_PERMISSIONS_BY_ROLE_ID] }
+      },
+      {
+        path: 'roles-edit/:id',
+        component: EditRoleComponent,
+        canActivate: [AuthGuard, ACLGuard],
+        data: { acl: [Permissions.GET_ALL_PERMISSIONS, Permissions.GET_ROLE_BY_ID] }
+      }
     ]
   }
 ];
--- AngularApplication/src/app/store/store-routing.module.ts
+++ AngularApplication/src/app/store/store-routing.module.ts
@@ -9,24 +9,30 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AlgoRunComponent } from './algo-run/algo-run.component';
 import { AlgoInstanceComponent } from './algo-instance/algo-instance.component';
 import { ACLGuard } from '../services/acl.guard';
+import Permissions from './models/permissions';
 
 
 const routes: Routes = [
   { path: '', component: StoreComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'algo-list', component: AlgoListComponent, canActivate: [AuthGuard, ACLGuard], data: { acl: ['GetAllAlgos'] } },
-  { path: 'algo-edit/:algoId', component: AlgoEditComponent, canActivate: [AuthGuard, ACLGuard], data: { acl: ['GetAlgoMetadata'] } },
+  { path: 'algo-list', component: AlgoListComponent, canActivate: [AuthGuard, ACLGuard], data: { acl: [Permissions.GET_ALL_ALGOS] } },
+  {
+    path: 'algo-edit/:algoId',
+    component: AlgoEditComponent,
+    canActivate: [AuthGuard, ACLGuard],
+    data: { acl: [Permissions.GET_ALGO_METADATA] }
+  },
   {
     path: 'algo-details/:clientId/:algoId', component: AlgoDetailsComponent,
-    canActivate: [AuthGuard, ACLGuard], data: { acl: ['GetUploadString', 'GetAlgoMetadata'] }
+    canActivate: [AuthGuard, ACLGuard], data: { acl: [Permissions.GET_UPLOAD_STRING, Permissions.GET_ALGO_METADATA] }
   },
   {
     path: 'algo-run/:clientId/:algoId', component: AlgoRunComponent,
-    canActivate: [AuthGuard, ACLGuard], data: { acl: ['GetUploadString', 'GetAlgoMetadata'] }
+    canActivate: [AuthGuard, ACLGuard], data: { acl: [Permissions.GET_UPLOAD_STRING, Permissions.GET_ALGO_METADATA] }
   },
   {
     path: 'algo-instance/:clientId/:algoId/:instanceId', component: AlgoInstanceComponent,
-    canActivate: [AuthGuard, ACLGuard], data: { acl: ['GetAlgoInstanceDataAsync', 'GetUploadString'] }
+    canActivate: [AuthGuard, ACLGuard], data: { acl: [Permissions.GET_ALGO_INSTANCE_DATA, Permissions.GET_UPLOAD_STRING] }
   },
 ];
 
