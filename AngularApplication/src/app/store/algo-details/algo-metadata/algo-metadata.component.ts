7afa91d3b3ba7904365690446efd35a7f31ede31
Todor Ivanov <todor.ivanov@primeholding.com>
2018-06-04 16:50:26 +0300

    Change the displayed value for enums

--- AngularApplication/src/app/store/algo-details/algo-metadata/algo-metadata.component.ts
+++ AngularApplication/src/app/store/algo-details/algo-metadata/algo-metadata.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { AlgoMetadata } from '../../models/algo-metadata.model';
 import { BaseAlgoParam } from '../../models/base-algo-param.model';
 
@@ -7,11 +7,31 @@ import { BaseAlgoParam } from '../../models/base-algo-param.model';
   templateUrl: './algo-metadata.component.html',
   styleUrls: ['./algo-metadata.component.scss']
 })
-export class AlgoMetadataComponent {
+export class AlgoMetadataComponent implements OnChanges {
 
   @Input() metadata: AlgoMetadata;
   @Output() onHighlight = new EventEmitter<BaseAlgoParam>();
   showAll: boolean;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['metadata'] && changes['metadata'].currentValue) {
+      this.metadata = changes['metadata'].currentValue;
+
+      this.metadata.Parameters.forEach(param => {
+        if (param['PredefinedValues'].length) {
+          param.Value = param['PredefinedValues'].find(p => p.Value === param.Value).Key;
+        }
+      });
+
+      this.metadata.Functions.forEach(func => {
+        func.Parameters.forEach(param => {
+          if (param['PredefinedValues'].length) {
+            param.Value = param['PredefinedValues'].find(p => p.Value === param.Value).Key;
+          }
+        });
+      });
+    }
+  }
 
   highlight(param: BaseAlgoParam): void {
     this.onHighlight.emit(param);
