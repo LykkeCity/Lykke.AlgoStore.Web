709489e474c3b14d4c9bd3064aeee0fb1211caae
Todor Ivanov <todor.ivanov@primeholding.com>
2018-06-15 15:39:18 +0300

    Fix commands and environment settings

--- AngularApplication/src/environments/environment.test.ts
+++ AngularApplication/src/environments/environment.test.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4200/devapi', // proxies requests to 'https://api-dev.lykkex.net/api'
-  apiAuthUrl: 'https://auth-dev.lykkex.net',
-  applicationId: '62b4815e-3762-4cf2-bad1-876a1feabeb7',
-  redirectUrl: 'http://localhost:4200/',
+  apiUrl: 'https://api-test.lykkex.net/api',
+  apiAuthUrl: 'https://auth-test.lykkex.net',
+  applicationId: '405712da-62b2-4231-85d3-de868a1e78a5',
+  redirectUrl: 'http://algo-store-web.algo-store.svc.cluster.local/',
 
-  storeApiUrl: 'http://algo-store-api.lykke-algo-store.svc.cluster.local/api',
-  apiV2Url: 'https://apiv2-dev.lykkex.net/api/',
-  authUrl: 'http://localhost:5000/api/home/authentication',
-  tokenUrl: 'https://auth-dev.lykkex.net/getlykkewallettoken',
+  storeApiUrl: 'http://algo-store-api.algo-store.svc.cluster.local/api',
+  apiV2Url: 'https://apiv2-test.lykkex.net/api/',
+  authUrl: 'http://algo-store-web.algo-store.svc.cluster.local/api/home/authentication',
+  tokenUrl: 'https://auth-test.lykkex.net/getlykkewallettoken',
   idleTime: 300,
   idleTimeout: 60
 };
